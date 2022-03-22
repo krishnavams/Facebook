2e287e48d57a7e98c5d38c83df51b3bf4253c4b2
krishnavams <35889193+krishnavams@users.noreply.github.com>
2022-03-22 07:56:47 +0530

    test3

--- Facebook/src/test/java/com/facebook/genaric/BaseClass.java
+++ Facebook/src/test/java/com/facebook/genaric/BaseClass.java
@@ -1,13 +1,9 @@
 package com.facebook.genaric;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.Test;
 
 public class BaseClass {
 	public static WebDriver driver;
-	@Test
-	public void FaceBookLogoDisplayed() {
-		
-	}
+	
 
 }
