1f3f2eb4b5dcdce21cfaea7bf70f3d07ffb2e725
krishnavams <35889193+krishnavams@users.noreply.github.com>
2022-03-24 06:55:18 +0530

    check

--- Facebook/src/test/java/com/facebook/genaric/BaseClass.java
+++ Facebook/src/test/java/com/facebook/genaric/BaseClass.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -16,8 +17,8 @@ public class BaseClass {
 	
 	@BeforeTest
 	public void openBrowser() {
-		WebDriverManager.edgedriver().setup();
-		driver = new EdgeDriver();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 	}
 	@BeforeMethod
